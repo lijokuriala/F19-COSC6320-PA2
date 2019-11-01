471b66a2e7ce2b3bbfbc1d7df2a36c2c165ff3d4
Lijo Kuriala <lijo.net@gmail.com>
2019-10-31 23:56:53 -0500

    Algv1.1

--- src/mainpkg/AlgorithmOne.java
+++ src/mainpkg/AlgorithmOne.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class AlgorithmOne {
 	
 	static int rows = 0, dimensions = 0;
-
+	static ArrayList<ArrayList<Float>> carPointsArray;
 	/* This method accepts input from user for points array */
 	public static void readInput(int rowCount, int dimensionCount, List<List<Float>> pointsArray) {
 		pointsArray.clear();
@@ -22,8 +22,8 @@ public class AlgorithmOne {
 	}
 
 	/* This method reads the input from a given file */
-	public static ArrayList<ArrayList<Float>> readInput(String fileName, int rowsInFile, int dimensionsInFile, ArrayList<ArrayList<Float>> pointsArray) {
-		pointsArray.clear(); /* Clear the points array */
+	public static ArrayList<ArrayList<Float>> readInput(String fileName) {
+		carPointsArray.clear(); /* Clear the points array */
 		int rowCounter = -999;
 		int dimensionCounter = -999;
 		try {
@@ -32,8 +32,8 @@ public class AlgorithmOne {
 
 			/* Read the first line of the file to get value of n and d */
 			try {
-				rows = rowsInFile = inputScanner.nextInt();
-				dimensions = dimensionsInFile = inputScanner.nextInt();
+				rows = inputScanner.nextInt();
+				dimensions = inputScanner.nextInt();
 				inputScanner.nextLine();
 			} catch (InputMismatchException e) {
 				System.out.println("First line of file is invalid. It must be integer values of n and d.");
@@ -44,16 +44,16 @@ public class AlgorithmOne {
 			 * values and stored in temp list This list is then added to points array
 			 */
 			rowCounter = 0;
-			while (inputScanner.hasNextLine() & ++rowCounter <= rowsInFile) {
+			while (inputScanner.hasNextLine() & ++rowCounter <= rows) {
 				String line = inputScanner.nextLine(); /* Read each line from file */
 				Scanner lineBreaker = new Scanner(line); /* Break up each line into parts */
 				dimensionCounter = 0;
 				ArrayList<Float> tempLine = new ArrayList<Float>(); /* Temp list of float for each row of file */
 				/* Iterating each token (float value) from the line */
-				while (lineBreaker.hasNext() & ++dimensionCounter <= dimensionsInFile) {
+				while (lineBreaker.hasNext() & ++dimensionCounter <= dimensions) {
 					tempLine.add(lineBreaker.nextFloat()); /* Add each token to temp list */
 				}
-				pointsArray.add(tempLine);	/*Add row to points array */
+				carPointsArray.add(tempLine);	/*Add row to points array */
 				lineBreaker.close();
 			}
 			inputScanner.close();
@@ -72,23 +72,69 @@ public class AlgorithmOne {
 		} finally {
 			// Close the file
 		}
-		return pointsArray;
+		return carPointsArray;
 	}
 
 	/*
 	 * This is the first algorithm to count the number of interesting points. It
 	 * return the count of interesting points.
 	 */
-	public static int countUsingAlgorithmOne(int numOfRows, int numOfDiamensions,ArrayList<ArrayList<Float>> pointsArray) {
+	public static int countUsingAlgorithmOne(int numOfRows, int numOfDiamensions, ArrayList<ArrayList<Float>> pointsArray) {
 		int interestingPointsCount = 0;
+		if (numOfRows < 2)
+			SimpleAlgo3(1, numOfRows, numOfDiamensions, pointsArray);
+		else if (numOfRows - numOfRows / 2 < 2)
+			 SimpleAlgo3(1, numOfRows, numOfDiamensions, pointsArray);
+		else {
+			SimpleAlgo3(1, (numOfRows / 2), numOfDiamensions, pointsArray);
+			SimpleAlgo3((numOfRows / 2 + 1), numOfRows, numOfDiamensions, pointsArray);
+		}
+		for (int i=0; i<numOfRows; i++)
+			if (carPointsArray.get(i).get(0) != -999.999f)
+				interestingPointsCount++;
 
 		return interestingPointsCount;
 	}
+	
+	public static void SimpleAlgo3(int start, int end, int d, ArrayList<ArrayList<Float>> pointsArray) {
+		System.out.println("start "+start);
+		System.out.println("end "+end);
+		for (int i = start -1; i < end; i++) {
+			for (int j = start -1; j < end; j++) {
+				System.out.println("i "+i);
+				if (carPointsArray.get(i).get(0) != -999.999f) {
+					boolean worse_somewhere = false;
+					for (int k = 0; k < d; k++)
+						if (pointsArray.get(i).get(k) < pointsArray.get(j).get(k)) {
+							worse_somewhere = true;
+//							System.out.println("i " + i + " j " + j + " k " + k);
+//							System.out.println("pointsArray.i.k < pointsArray.j.k::::: " + pointsArray.get(i).get(k) + " :: " + pointsArray.get(j).get(k));
+						}
 
+					boolean not_better = true;
+					for (int k = 0; k < d; k++)
+						if (pointsArray.get(i).get(k) > pointsArray.get(j).get(k)) {
+							not_better = false;
+//							System.out.println("i "+i+" j "+j+ " k "+k);
+//							System.out.println("pointsArray.i.k > pointsArray.j.k::::: "+pointsArray.get(i).get(k) +" :: "+ pointsArray.get(j).get(k)); 
+						}
+
+					if (worse_somewhere && not_better) {
+//						System.out.println(pointsArray.get(i));
+						pointsArray.get(i).set(0, -999.999f);
+					}
+				}
+			}
+		}
+		/*//Printing interesting points
+		 * for (int i=0; i<n; i++) if (isBest.get(i)) { for (int k=0; k<d; k++)
+		 * System.out.print(pointsArray.get(i).get(k) + ""); System.out.println(); }
+		 */
+	}
+	
 	/* Sample algorithm given by professor */
 	public static int SimpleAlgo2(int n, int d, ArrayList<ArrayList<Float>> pointsArray) {
 		List<Boolean> isBest = new ArrayList<Boolean>(Collections.nCopies(n + 10, true));
-		System.out.println(n + " is the value of n");
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if (isBest.get(i)) {
@@ -110,7 +156,8 @@ public class AlgorithmOne {
 
 					if (worse_somewhere && not_better) {
 						isBest.set(i, false);
-						System.out.println(pointsArray.get(i));
+//						System.out.println(pointsArray.get(i));
+						pointsArray.get(i).set(0, -999.999f);
 					}
 				}
 			}
@@ -129,21 +176,41 @@ public class AlgorithmOne {
 	
 	/* Main method */
 	public static void main(String[] args) throws InterruptedException {
-		ArrayList<ArrayList<Float>> carPointsArray = new ArrayList<ArrayList<Float>>(); // This holds the dimensional array
+		carPointsArray = new ArrayList<ArrayList<Float>>(); // This holds the dimensional array
+		long start, end;
 		
 //		readInput(5,2); /* This can be used for small input set */
 		String filePath = System.getProperty("user.dir") + "\\src\\mainpkg\\" + "1.in"; /* Specify your file name here */
-		carPointsArray = readInput(filePath, rows, dimensions, carPointsArray);
-		long start = System.nanoTime(); /* Record starting time */
-
-		System.out.println("Number of interesting points using SimpleAlgo2 : "
-				+ SimpleAlgo2(rows, dimensions, carPointsArray)); /* Calling SimpleAlgo2 */
-		long end = System.nanoTime(); /* Record ending time */
-		System.out.println("Time taken by SimpleAlgo2 is " + (end-start) + " nanoseconds!");
-
-		// JOptionPane.showMessageDialog(null,"Time taken by Algorithm one is " +
-		// timeTaken + " nanoseconds!");
 		
+		/* Read and populate the Array */
+		carPointsArray = readInput(filePath);
+		System.out.println("Number of rows: "+rows);
+		System.out.println("Number of dimensions: "+dimensions);
+		
+/*		// Record starting time 
+		start = System.nanoTime(); 
+		// Calling SimpleAlgo2 
+		System.out.println("\nNumber of interesting points using SimpleAlgo2 : "+ SimpleAlgo2(rows, dimensions, carPointsArray));
+		// Record ending time 
+		end = System.nanoTime(); 
+		System.out.println("Time taken by SimpleAlgo2 is " + (end-start) + " nanoseconds!");*/
+		
+		// Record starting time
+		start = System.nanoTime();
+		// Calling SimpleAlgo2
+		System.out.println("\nNumber of interesting points using AlgorithmOne : " + countUsingAlgorithmOne(rows, dimensions, carPointsArray));
+		// Record ending time
+		end = System.nanoTime();
+		System.out.println("Time taken by AlgorithmOne is " + (end - start) + " nanoseconds!");
+
+		// Printing interesting points
+		System.out.println("\n\nInteresting points are : ");
+		for (int i = 0; i < rows; i++)
+			if (carPointsArray.get(i).get(0) != -999.999f) {
+				for (int k = 0; k < dimensions; k++)
+					System.out.print(carPointsArray.get(i).get(k) + "\t");
+				System.out.println();
+			} 
 
 	}
 
